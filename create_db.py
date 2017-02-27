79ac448fba56be8eee1cbd6f012209a323092b1d
lior13 <lior16189@gmail.com>
2017-02-27 18:16:50 +0200

    added few scripts and made few changes

--- create_db.py
+++ create_db.py
@@ -4,4 +4,3 @@ connection = pymysql.connect(host="localhost", user="root", charset='utf8')
 cursor = connection.cursor()
 sql = 'CREATE DATABASE project'
 cursor.execute(sql)
-
--- football/run_football.py
+++ football/run_football.py
@@ -1,4 +1,4 @@
-files_to_run = [r"nfl.py", r"data-american-football.py", r"spain-01-archive.py"]
+files_to_run = [r"nfl.py", r"data-american-football.py"]
 
 for file in files_to_run:
     execfile(file)
